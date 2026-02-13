e3f92828ba85388236144778795318729854f1c1
Karthik <karpn98@gmail.com>
2026-02-13 17:56:37 +0530

    1. Vibe coding Frontend setup 2. Setup OIDC Auth with Auth0

---
+++ Chat/Components/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Components.Controllers
+{
+    [Route("account")]
+    public class LoginController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = returnUrl
+            }, "Auth0");
+        }
+
+        [HttpGet("logout")]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
+            {
+                RedirectUri = Url.Content("~/")
+            });
+        }
+    }
+}
